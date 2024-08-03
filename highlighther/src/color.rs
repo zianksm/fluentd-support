7c35f52bcbc492788008a78575f5102c3b85cf27
zianksm <mynameiszian@gmail.com>
2024-08-03 22:55:34 +0800

    feat: add color

---
+++ highlighther/src/color.rs
@@ -0,0 +1,21 @@
+pub trait Color {
+    fn color(&self) -> &'static str;
+}
+
+#[macro_export]
+macro_rules! color {
+    ($($color:ident)*) => {
+        $(
+            paste::paste!{
+                pub struct $color;
+                impl Color for $color {
+                    fn color(&self) -> &'static str {
+                        stringify!([<$color:lower>])
+                    }
+                }
+            }
+        )*
+    };
+}
+
+color!(Red);
--- highlighther/src/lib.rs
+++ highlighther/src/lib.rs
@@ -1,3 +1,2 @@
-fn main() {
-    println!("Hello, world!");
-}
+mod keyword;
+mod color;
